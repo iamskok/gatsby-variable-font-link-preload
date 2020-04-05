0ce33b707cea6ed3ac5ab7ee9c8f847e4adbeb86
Vladimir <vladimir.skok@bvaccel.com>
2020-04-05 16:34:34 -0700

    fix: move styles from Layout => gatsby-browser.js

---
+++ gatsby-browser.js
@@ -0,0 +1,1 @@
+import './src/components/font-face-stacks'
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import FontFaceStacks from './font-face-stacks'
 
 const Layout = ({ children }) => (
   <>
-    <FontFaceStacks />
     <main>
       {children}
     </main>
